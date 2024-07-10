3661cb16b2d981e597839d2432df7bfb376a9797
Antonio Diaz Flores <wisky.brandy@hotmail.com>
2024-07-10 01:48:11 -0700

    Basic nvim config

--- user/app/terminal/tmux.nix
+++ user/app/terminal/tmux.nix
@@ -19,11 +19,11 @@ in
       type = types.attrs;
       default = {
         accent = "darkblue";
-	dimAccent = "blue";
-	accentForeground = "white";
-	background = "cyan";
-	foreground = "black";
-	dimForeground = "darkgray";
+        dimAccent = "blue";
+        accentForeground = "white";
+        background = "cyan";
+        foreground = "black";
+        dimForeground = "darkgray";
       };
       description = "tmux colors";
     };
@@ -43,42 +43,42 @@ in
       extraConfig = ''
         # Bindings
 
-	## Reload tmux config
-	bind C-r source ~/.config/tmux/tmux.conf; display "Sourced ~/.config/tmux/tmux.conf"
+        ## Reload tmux config
+        bind C-r source ~/.config/tmux/tmux.conf; display "Sourced ~/.config/tmux/tmux.conf"
 
-	# Misc
+        # Misc
 
-	## Focus events enabled for terminals that support them
+        ## Focus events enabled for terminals that support them
         set -g focus-events on
 
-	# Style
-
-	## Counting
-	set -g base-index 1
+        # Style
+      
+        ## Counting
+        set -g base-index 1
         setw -g pane-base-index 1
-	
-	## Titles
+        
+        ## Titles
         set -g set-titles on
-
-	## Timing
-
-	### Update status bar every second instead of every 15. Likely uses more energy
+      
+        ## Timing
+      
+        ### Update status bar every second instead of every 15. Likely uses more energy
         set -g status-interval 1
-	### Display panes for 3 seconds instead of 1
-	set -g display-panes-time 3000
+        ### Display panes for 3 seconds instead of 1
+        set -g display-panes-time 3000
 
         ## Status bar
-	set -g status-left-length 25
-	set -g status-right ' "#{=21:pane_title}" %H:%M:%S %d-%b-%y'
-
-	## Colors
+        set -g status-left-length 25
+        set -g status-right ' "#{=21:pane_title}" %H:%M:%S %d-%b-%y'
+      
+        ## Colors
         set -g status-style 'fg=${cfg.colors.foreground}, bg=${cfg.colors.background}'
 
-	set -g window-status-current-style 'fg=${cfg.colors.accentForeground} bg=${cfg.colors.accent}'
+        set -g window-status-current-style 'fg=${cfg.colors.accentForeground} bg=${cfg.colors.accent}'
 
         set -g pane-border-style 'fg=${cfg.colors.background}'
         set -g pane-active-border-style 'fg=${cfg.colors.dimAccent}'
-	
+  
         set -g message-style 'fg=${cfg.colors.accentForeground}, bg=${cfg.colors.accent}'
   
         set -g mode-style 'fg=${cfg.colors.background}, bg=${cfg.colors.accent}'
